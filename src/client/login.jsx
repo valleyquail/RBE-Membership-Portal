0c893cce25c444b160410ecc59088a91001915bf
Nikesh Walling <nikeshwalling@gmail.com>
2023-11-03 13:33:06 -0400

    Adding JS dev branch

--- src/client/login.jsx
+++ src/client/login.jsx
@@ -10,26 +10,6 @@ function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const user = {
-      username: username,
-      password: password,
-    };
-    const response = await fetch("/findUser", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(user),
-    });
-    const text = await response.text();
-    const validation = JSON.parse(text);
-    if (validation == "true") {
-      console.log("Normal login");
-      window.location.href = "/login";
-    } else {
-      console.log("failed to log in");
-    }
-  };
   const githubOAuth = (event) => {
     console.log("githubbing");
     window.location.href = "/auth/github";
@@ -38,6 +18,11 @@ function Login() {
   const googleOAuth = (event) => {
     console.log("googling");
     window.location.href = "/auth/google";
+  };
+
+  const microsoftOAuth = (event) => {
+    console.log("microsofting");
+    window.location.href = "/auth/microsoft";
   };
   return (
     <div>
@@ -63,42 +48,16 @@ function Login() {
           </Modal.Header>
 
           <Modal.Body className="p-5 pt-0">
-            <Form>
-              <Form.Floating className="mb-3">
-                <Form.Control
-                  type="email"
-                  className="rounded-3"
-                  name="emailBox"
-                  placeholder="name@example.com"
-                  autoComplete="username"
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-                <label htmlFor="emailBox">Email address</label>
-              </Form.Floating>
-              <Form.Floating className="mb-3">
-                <Form.Control
-                  type="password"
-                  className="rounded-3"
-                  name="passwordBox"
-                  placeholder="Password"
-                  autoComplete="current-password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <label htmlFor="passwordBox">Password</label>
-              </Form.Floating>
-              <Button
-                variant="primary"
-                type="submit"
-                name="sign_in_button"
-                className="w-100 mb-2 rounded-3"
-                size="lg"
-                onClick={handleSubmit}
-              >
-                Sign In
-              </Button>
-            </Form>
             <hr className="my-4" />
             <h2 className="fs-5 fw-bold mb-3">Or use a third-party</h2>
+            <Button
+              variant="secondary"
+              id="githubButton"
+              className="w-100 py-2 mb-2 rounded-3"
+              onClick={microsoftOAuth}
+            >
+              Sign in with Microsoft
+            </Button>
             <Button
               variant="secondary"
               id="googleButton"
